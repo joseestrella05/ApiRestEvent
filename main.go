086078bace03c0f8cfd3afc2af9f4be5df3da23d
Jestrella <josegabrilestrella05@gmail.com>
2026-07-13 10:23:57 -0400

    Implement basic server setup with GET endpoint for events

--- main.go
+++ main.go
@@ -1,9 +1,18 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	gin.Default()
+	server := gin.Default()
+
+	server.GET("/events", getEvents) //GET, POST, PUT, PATCH, DELETE
+	server.Run(":8080")              //localhost:8080
+}
+
+func getEvents(context *gin.Context) {
+	context.JSON(http.StatusOK, gin.H{"message": "hello!"})
 }
