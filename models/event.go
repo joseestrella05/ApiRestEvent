a4fdbff10bccba7b17352c1d9af1dcf2acbdda5e
Jestrella <josegabrilestrella05@gmail.com>
2026-07-13 13:02:00 -0400

    Add HTTP request for creating an event and fix typo in Event model

--- models/event.go
+++ models/event.go
@@ -5,7 +5,7 @@ import "time"
 type Event struct {
 	ID          int
 	Name        string    `binding:"required"`
-	Descripcion string    `binding:"required"`
+	Description string    `binding:"required"`
 	Location    string    `binding:"required"`
 	DateTime    time.Time `binding:"required"`
 	UserId      int
