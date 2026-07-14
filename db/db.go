9c7dc1761025f67f89f25f1e6a05605dfa90c0d8
Jestrella <josegabrilestrella05@gmail.com>
2026-07-14 16:52:55 -0400

    Add user table creation and signup route to the database and routes

--- db/db.go
+++ db/db.go
@@ -24,6 +24,18 @@ func InitDB() {
 }
 
 func createTables() {
+	createUserTable := `
+	CREATE TABLE IF NOT EXISTS users(
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		email TEXT NOT NULL UNIQUE,
+		password TEXT NOT NULL
+	)
+	`
+	_, err := DB.Exec(createUserTable)
+
+	if err != nil {
+		panic("could not created user table")
+	}
 	createEventsTable := `
 	CREATE TABLE IF NOT EXISTS events (
 		id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -32,10 +44,11 @@ func createTables() {
 		location TEXT NOT NULL,
 		dateTime DATETIME NOT NULL,
 		user_id INTEGER
+		FOREIGN KEY(user_id) REFERENCES users(id)
 	)
 	`
 
-	_, err := DB.Exec(createEventsTable)
+	_, err = DB.Exec(createEventsTable)
 
 	if err != nil {
 		panic("Could not create events table: " + err.Error())
