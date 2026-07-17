c2c0b43be5051d7d19530732ec3e7b98f171cc5a
Jestrella <josegabrilestrella05@gmail.com>
2026-07-17 11:45:05 -0400

    Fix JWT token generation method and enhance token verification logic

--- utils/jwt.go
+++ utils/jwt.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"errors"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
@@ -9,12 +10,43 @@ import (
 const secretKey = "supersecret"
 
 func GenerateToken(email string, userId int64) (string, error) {
-	token := jwt.NewWithClaims(jwt.SigningMethodES256, jwt.MapClaims{
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
 		"email":  email,
 		"userId": userId,
 		"exp":    time.Now().Add(time.Hour * 2).Unix(),
 	})
 
-	return token.SignedString(secretKey)
+	return token.SignedString([]byte(secretKey))
+}
 
+func VerifyToken(token string) error {
+	parsedToken, err := jwt.Parse(token, func(t *jwt.Token) (interface{}, error) {
+		_, ok := t.Method.(*jwt.SigningMethodHMAC)
+
+		if !ok {
+			return nil, errors.New("Unexpected signing method")
+		}
+
+		return []byte(secretKey), nil
+	})
+
+	if err != nil {
+		return errors.New("Could not parse token")
+	}
+
+	tokenIsvalid := parsedToken.Valid
+
+	if !tokenIsvalid {
+		return errors.New("Invalid token!")
+	}
+	//claims, ok := parsedToken.Claims.(jwt.MapClaims)
+
+	//if !ok {
+	//	return errors.New("Invalid token claims.")
+	//}
+
+	//email := claims["email"].(string)
+	//userId := claims["userId"].(int64)
+
+	return nil
 }
